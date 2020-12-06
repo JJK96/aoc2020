a771bc35d025f16bbd0f6e02b56e7810979379c9
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2020-12-06 11:36:36 +0100

    Without mutable

--- 1/main.rkt
+++ 1/main.rkt
@@ -2,19 +2,16 @@
 (require racket/set)
 
 (define (find-sum numbers sum)
-  (let ([num-set (mutable-set)])
-    (define (loop numbers)
-      (if (null? numbers)
-        '()
-        (let*
-          ( [num (car numbers)]
-            [diff (- sum num)])
-          (cond 
-            [(set-member? num-set diff) (list diff num)]
-            [else 
-               (set-add! num-set num)
-               (loop (cdr numbers))]))))
-    (loop numbers)))
+  (define (loop numbers num-set)
+    (if (null? numbers)
+      '()
+      (let*
+        ( [num (car numbers)]
+          [diff (- sum num)])
+        (if (set-member? num-set diff) 
+          (list diff num)
+          (loop (cdr numbers) (set-add num-set num))))))
+  (loop numbers (set)))
 
 (apply * (find-sum
             (for/list ([line (file->lines "input")])

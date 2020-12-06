0819ff8453ec69e01d9936d2dab3bfaf4ea764e5
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2020-12-06 16:35:42 +0100

    calculate part 2

--- 1/main.rkt
+++ 1/main.rkt
@@ -13,7 +13,21 @@
           (loop (cdr numbers) (set-add num-set num))))))
   (loop numbers (set)))
 
-(apply * (find-sum
-            (for/list ([line (file->lines "input")])
-               (string->number line))
-            2020))
+(define (find-sum3 numbers sum)
+  (for*/first
+    ( [a numbers]
+      [b numbers]
+      [c numbers]
+      #:when (= sum (+ a b c)))
+    (list a b c)))
+
+(define numbers 
+  (for/list ([line (file->lines "input")])
+       (string->number line)))
+
+(define (fun1) (apply * (find-sum numbers 2020)))
+
+(define (fun2) (apply * (find-sum3 numbers 2020)))
+
+(fun1)
+(fun2)
